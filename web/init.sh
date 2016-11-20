8cbfce61a829437be741d585c785db23937eec2c
ShidenKai <rusdiablo666@gmail.com>
2016-11-20 19:35:58 +0500

    init and gunicorn conf fix

--- web/init.sh
+++ web/init.sh
@@ -1,4 +1,5 @@
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicornd.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicornd.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
